ea3d8d4d6d39f04c378753d9a982303e14b74e7c
Elisa Guex Pertuso <elisaguex@gmail.com>
2020-08-12 21:29:39 -0300

    Update products.js

--- js/products.js
+++ js/products.js
@@ -1,6 +1,33 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
+
+
+var categoriesArray = [];
+
+function showCategoriesList(array){
+
+    let htmlContentToAppend = "";
+    for(let i = 0; i < array.length; i++){
+        let category = array[i];
+
+        htmlContentToAppend += `
+        <div class="list-group-item list-group-item-action">
+            <div class="row" > 
+
+                <div class="col-3">
+                    <img src="` + category.imgSrc + `" alt="` + category.desc + `" class="img-thumbnail">
+                </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <h4 class="mb-1">`+ category.name +`</h4>
+                        <small class="text-muted">` + category.productCount + ` artículos</small>
+                    </div>
+                     <div>` + category.description + `</div> 
+                </div>
+            </div>
+        </div> 
+        `
 document.addEventListener("DOMContentLoaded", function (e) {
 
 });
