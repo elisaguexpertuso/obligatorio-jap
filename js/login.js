5096315bf0da6ce8fa6601adde7b81cfff9ad302
Elisa Guex Pertuso <elisaguex@gmail.com>
2020-08-14 08:21:14 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,6 +1,34 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+const email = document.getElementById("email");
+const password = document.getElementById("password")
+const form = document.getElementById("mi_formulario")
+const parrafo = document.getElementById("warnings")
 
-});
+form.addEventListener("submit", e=>{
+  e.preventDefault()
+  let warnings =""
+  let entrar=false
+  let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+  parrafo.innerHTML= ""
+
+  if(!regexEmail.test(email.value)){
+    warnings += `Ingresar email <br>`
+    entrar = true
+}
+  if(password.value.length <1){
+    warnings += `Ingresar contraseña <br>`
+    entrar = true
+  }
+
+  if (entrar){
+    parrafo.innerHTML = warnings
+  } else{
+    document.querySelector("#redireccionar")
+    .addEventListener("click", () =>{
+        window.location.href = "index.html";
+    });
+     
+  }
+})
