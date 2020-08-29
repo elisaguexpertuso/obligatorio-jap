a857270058a0adae903fbd1bc139e5b5be0badf6
Elisa Guex Pertuso <elisaguex@gmail.com>
2020-08-29 18:51:14 -0300

    master

--- js/init.js
+++ js/init.js
@@ -44,4 +44,6 @@ var getJSONData = function(url){
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
+  sessionStorage.getItem("Usuario activo");
+  document.getElementById("logged").innerHTML = sessionStorage.getItem("Usuario activo");
 });
--- js/login.js
+++ js/login.js
@@ -1,23 +1,24 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-const email = document.getElementById("email");
-const password = document.getElementById("password")
 const form = document.getElementById("mi_formulario")
 const parrafo = document.getElementById("warnings")
 
 form.addEventListener("submit", e=>{
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+  
   e.preventDefault()
   let warnings =""
   let entrar=false
   let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
   parrafo.innerHTML= ""
 
-  if(!regexEmail.test(email.value)){
+  if(!regexEmail.test(email)){
     warnings += `Ingresar email <br>`
     entrar = true
 }
-  if(password.value.length <1){
+  if(password.length <1){
     warnings += `Ingresar contraseña <br>`
     entrar = true
   }
@@ -25,8 +26,7 @@ form.addEventListener("submit", e=>{
   if (entrar){
     parrafo.innerHTML = warnings
   } else{
-            window.location.href = "ex-index.html";
+      sessionStorage.setItem("Usuario activo",email);
+           window.location.href = "ex-index.html";
     };
-     
-  
 });
