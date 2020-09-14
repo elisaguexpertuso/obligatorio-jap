cdc59f1f07220ca78f2e5f1ab8f93ec68918c7a3
Elisa Guex Pertuso <elisaguex@gmail.com>
2020-09-13 21:05:10 -0300

    Update product-info.js

--- js/product-info.js
+++ js/product-info.js
@@ -1,6 +1,116 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+var product = {};
+var comments = {};
 
+
+
+function showProductGallery(array){
+
+    let htmlContentToAppend = "";
+
+    for(let i = 0; i < array.length; i++){
+        let imageSrc = array[i];
+
+        htmlContentToAppend += `
+        <div class="col-lg-3 col-md-4 col-6">
+            <div class="d-block mb-4 h-100">
+                <img class="img-fluid img-thumbnail" src="` + imageSrc + `" alt="">
+            </div>
+        </div>
+        `
+
+        document.getElementById("product-info-id").innerHTML = htmlContentToAppend;
+    }
+}
+document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(PRODUCT_INFO_URL).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+            product = resultObj.data;
+
+            let productNameHTML  = document.getElementById("productName");
+            let productDescriptionHTML = document.getElementById("productDescription");
+            let productCountHTML = document.getElementById("costo");
+            let productCriteriaHTML = document.getElementById("categoría");
+        
+            productNameHTML.innerHTML = product.name;
+            productDescriptionHTML.innerHTML = product.description;
+            productCountHTML.innerHTML = product.cost + product.currency;
+            productCriteriaHTML.innerHTML = product.category;
+
+            //Muestro las imagenes en forma de galería
+            showProductGallery(product.images);
+        }
+    });
+});
+
+
+
+function nuevocomentario(){
+    var comentario = document.getElementById("comentario").value;
+    var score = document.getElementById("score").value;
+
+    if (score == ""){
+    document.getElementById("calificacion").innerHTML ="Ingresa una calificación :)";
+    } 
+    if (comentario == "") {
+    document.getElementById("errorComentario").innerHTML="Ingresa un comentario :)";
+
+    } else {
+
+    document.getElementById("enviado").innerHTML= "Su comentario ha sido enviado ¡Gracias!";
+    document.getElementById("refresh").remove();
+    };
+};
+
+function showComments(comments){
+
+ 
+
+    let htmlContentToAppend ="";
+
+    for(let i = 0; i < comments.length; i++){
+        let commentario = comments[i];
+
+    htmlContentToAppend += `
+    
+    <div class="container">
+    <div class="card border-muted mb-4" class="mx-auto>   
+    <div class = "text-center" style="max-width: 40rem;"> 
+  
+   
+             <div class="col">                               
+        <small class="text-dark"> ` + "Puntuación:  " + commentario.score +  `</small>
+         </div>  
+
+         <div class="col">                                       
+                     <small class="text-muted">` + commentario.description + `</small>                 
+                </div>   
+
+                <div class="col">                                       
+                <small class="text-primary">` + "Usuario: " + commentario.user + `</small>                 
+           </div>    
+
+           <div class="col">                                       
+                <small class="text-muted">` + "Hora: " + commentario.dateTime + `</small>                 
+           </div> 
+    </div>
+    </div>  
+    </div>
+     
+    
+    `
+    
+    document.getElementById("comments").innerHTML = htmlContentToAppend;
+}
+}
+
+document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+            comments = resultObj.data;
+
+            showComments(comments);
+        }
+    });
 });
